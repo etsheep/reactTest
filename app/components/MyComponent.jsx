c4939c24e505caad8ada7b7550a6c54be47fcb77
huangjinyang <huangjinyang@gzac.org>
2017-08-26 16:09:23 +0800

    react

---
+++ app/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello World111111111111111!</div>
+        )
+    }
+}
+
+//导出组件
+export default MyComponent;
